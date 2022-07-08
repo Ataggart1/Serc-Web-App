6dd5d0231799986ad4d6e061bf53a46b734f62cc
Ataggart1 <50051852@belfastmet.ac.uk>
2022-07-08 14:17:06 +0100

    test 2

--- InternetExperiment/scripts.js
+++ InternetExperiment/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://icy-plant-007ad6003.1.azurestaticapps.net', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
